d1e09e8dbb81d17bce710c4ec28d616d73389d0d
Detrol <thuno@live.se>
2023-09-12 10:25:06 +0200

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    @if ((request()->getHost() != 'localhost'))
     <!-- Start cookieyes banner -->
     <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/0b31c6d330d72ed32b2e2498/script.js"></script>
     <!-- End cookieyes banner -->
+    @endif
 
     <!-- Title -->
     <title>Fixihem</title>
